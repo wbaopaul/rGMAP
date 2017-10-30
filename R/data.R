53030e35f13a8b947068b5cd1f8a10f5a0d759fe
Yu <yuw1@reslntan02.chop.edu>
2017-10-30 14:00:44 -0400

    document example data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Normalized Hi-C data for IMR90, chr15 with resolution 10kb.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data table with 3 variables:
+#' \describe{
+#'   \item{n1}{bin 1}
+#'   \item{n2}{bin 2}
+#'   \item{count}{normalized counts}
+#' }
+#' @source Rao et al., Cell 2014, A 3D map of the human genome at kilobase resolution reveals principles of chromatin looping
+"hic_rao_IMR90_chr15"
